ddac004356b773abc0eba843513f9998346bdb54
Misgar-Numan <numanasgar10@gmail.com>
2024-10-01 22:23:55 +0530

    Fix SVG Typo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('generateButton').addEventListener('click', function () {
   const qrText = document.getElementById('qrText').value;
-  const qrSize = parseInt(document.getElementById('qrSize').value) || 200; //Default Size: 200
+  const qrSize = parseInt(document.getElementById('qrSize').value) || 200; // Default Size: 200
   const qrCodeContainer = document.getElementById('qrCode');
 
   qrCodeContainer.innerHTML = ''; // Clear previous QR Code
@@ -19,17 +19,30 @@ document.getElementById('generateButton').addEventListener('click', function () 
     const downloadButton = document.getElementById('downloadButton');
     downloadButton.style.display = 'block';
 
-    // Download QR code as an image
+    // Download QR code
     downloadButton.onclick = function () {
-      const format = document.getElementById('qrFormat').value || 'png'; //Default Format: 'png'
+      const format = document.getElementById('qrFormat').value || 'png'; // Default Format: 'png'
       let mimeType;
+      let dataURL;
+
       if (format === 'png') {
         mimeType = 'image/png';
-      } else {
+        dataURL = qr.canvas.toDataURL(mimeType);
+      } else if (format === 'jpeg') {
         mimeType = 'image/jpeg';
+        dataURL = qr.canvas.toDataURL(mimeType);
+      } else if (format === 'svg') {
+        mimeType = 'image/svg+xml';
+        const svgData = `
+          <svg xmlns="http://www.w3.org/2000/svg" width="${qrSize}" height="${qrSize}">
+            <rect width="100%" height="100%" fill="white"/>
+            <path d="${qr.toSVG()}" fill="black"/>
+          </svg>`;
+        dataURL = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svgData);
       }
+
       const link = document.createElement('a');
-      link.href = qr.canvas.toDataURL(mimeType);
+      link.href = dataURL;
       link.download = `QRazyCode.${format}`;
       link.click();
     };
